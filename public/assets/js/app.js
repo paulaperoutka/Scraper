3a7229fe473efedd03f482c6ead09f9c77099fca
paulaperoutka <paulaperoutka@gmail.com>
2018-04-17 13:10:40 -0400

    unsave an article functional

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,3 +1,4 @@
+//Scrape
 $("#scrape").on("click", () => {
   $.ajax({
     method: "GET",
@@ -8,16 +9,24 @@ $("#scrape").on("click", () => {
   });
 });
 
-//no ES6, this = window b/c of scope inheritance
+//Save, no ES6, this = window b/c of scope inheritance
 $(".save").on("click", function () {
-  let thisId = $(this).attr("data-id");
+  let thisArticle = $(this).attr("data-id");
   $.ajax({
     method: "POST",
-    url: "/articles/save/" + thisId
+    url: "/articles/save/" + thisArticle
   }).done((data) => {
     window.location = "/saved";
   });
 });
 
-
-
+//Unsave, no ES6
+$(".unsave").on("click", function () {
+  const thisArticle = $(this).attr("data-id");
+  $.ajax({
+    method: "POST",
+    url: "/articles/delete/" + thisArticle
+  }).done((data) => {
+    window.location = "/saved";
+  });
+});
